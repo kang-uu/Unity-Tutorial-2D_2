9cfc1e468c85c1d88ad3b8e50e88f459cb598e3d
JeongDay <cidd1332@gmail.com>
2025-06-04 15:10:26 +0900

    2025.06.04 오후시간 1

--- Assets/02. Scripts/Cat/SoundManager.cs
+++ Assets/02. Scripts/Cat/SoundManager.cs
@@ -18,7 +18,7 @@ namespace Cat
             audioSource.clip = bgmClip; // 오디오 소스에 사운드 파일 설정
             audioSource.playOnAwake = true; // 시작할 때 자동 재생
             audioSource.loop = true; // 반복 기능
-            audioSource.volume = 0.1f; // 소리 음량
+            audioSource.volume = 0.02f; // 소리 음량
             
             audioSource.Play(); // 시작
         }
