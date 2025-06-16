3797464546c715eae1ac686d21be9aebceb1fb61
JeongDay <cidd1332@gmail.com>
2025-06-16 16:13:29 +0900

    2025.06.16 월요일 오후시간
    - 오버로딩
    - 상속
    - 가상화 / 추상화 / 오버라이드
    - 인터페이스

---
+++ Assets/02. Scripts/OOP/Interface/FlashLight.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class FlashLight : MonoBehaviour, IDropItem
+{
+    public void Grab()
+    {
+        Debug.Log("손전등을 주웠다.");
+        Destroy(gameObject);
+    }
+
+    public void Use()
+    {
+        Debug.Log("라이트를 켠다.");
+    }
+
+    public void Drop()
+    {
+        Debug.Log("손전등을 버렸다.");
+    }
+}
--- Assets/02. Scripts/OOP/Monster.cs
+++
@@ -1,6 +0,0 @@
-﻿using UnityEngine;
-
-public class Monster : MonoBehaviour
-{
-    
-}
