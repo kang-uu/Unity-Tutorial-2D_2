249eee50359b9a9f08567184cddc367650635a81
Gang <kingrkddn@gmail.com>
2025-06-30 16:00:57 +0900

    ad
    wer

--- Assets/02. Scripts/Knight/InteractionEvent.cs
+++ Assets/02. Scripts/Knight/InteractionEvent.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using UnityEngine;
 
 public class InteractionEvent : MonoBehaviour
@@ -6,7 +7,18 @@ public class InteractionEvent : MonoBehaviour
     public enum InteractionType { SIGN, DOOR, NPC }
     public InteractionType type;
 
-    public GameObject signPopup;
+    public SoundController soundController;
+
+    public GameObject popUp;
+
+    public FadeRoutine fade;
+    
+    public GameObject map;
+    public GameObject house;
+
+    public Vector3 inDoorPos;
+    public Vector3 outDoorPos;
+    public bool isHouse;
 
     void OnTriggerEnter2D(Collider2D other)
     {
@@ -16,19 +28,48 @@ public class InteractionEvent : MonoBehaviour
         }
     }
 
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            popUp.SetActive(false);
+        }
+    }
+
     void Interaction(Transform player)
     {
         switch (type)
         {
             case InteractionType.SIGN:
-                signPopup.SetActive(true);
+                popUp.SetActive(true);
                 break;
             case InteractionType.DOOR:
-                
+                StartCoroutine(DoorRoutine(player));
                 break;
             case InteractionType.NPC:
-                
+                popUp.SetActive(true);
                 break;
         }
+    }
+
+    IEnumerator DoorRoutine(Transform player)
+    {
+        soundController.EventSoundPlay("Door Open");
+        
+        yield return StartCoroutine(fade.Fade(3f, Color.black, true));
+
+        map.SetActive(isHouse);
+        house.SetActive(!isHouse);
+
+        var pos = isHouse ? outDoorPos : inDoorPos; 
+        player.transform.position = pos;
+        
+        isHouse = !isHouse;
+        
+        soundController.EventSoundPlay("Door Close");
+        
+        yield return new WaitForSeconds(1f);
+        yield return StartCoroutine(fade.Fade(3f, Color.black, false));
+
     }
 }
